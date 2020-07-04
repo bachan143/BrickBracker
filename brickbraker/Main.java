9ec0e4bfb71a2376cabb08ae1fcc3b28754a7da5
bachan <ghatuarybachan@gmail.com>
2020-07-04 17:07:41 +0530

    first Commit

---
+++ brickbraker/Main.java
@@ -0,0 +1,20 @@
+package com.brickbraker;
+
+import javax.swing.JFrame;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		
+		JFrame obj=new JFrame();
+		GamePlay gp=new GamePlay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gp);
+	}
+
+}
